e57ff1331f57dc3467d9a8e4e96581ba8fd77d2e
abraham <fcoabrahamprz@gmail.com>
2023-05-01 11:34:04 -0700

    List of files used updated when user ask a question

--- main.py
+++ main.py
@@ -30,15 +30,15 @@ with gr.Blocks() as demo:
     msg = gr.Textbox(placeholder="Type your message here")
     chatbot = gr.Chatbot()
     clear = gr.Button("Clear")
+    files_used = gr.Files(value=[], label="Files used")
+    
     
     def user(user_message, history):
         return user_message, history + [[user_message, None]]
 
-    def bot(subject, grade, user_message, history):
+
+    def bot(subject, grade, user_message, history, files_used):
         pdf_paths = []
-        
-        pdf_paths.append(os.path.join(AZ_STANDARDS, 'base', 'lesson_plan_template.pdf'))
-        # The template provided allows the LM to understand the context of the question and provide an example for a lesson plan.
         
         subject_selection, grade_selection = SUBJECT_LIST.index(subject), GRADE_LIST.index(grade)
         
@@ -53,11 +53,14 @@ with gr.Blocks() as demo:
         
         grade_path = subject_dir[grade_selection+1]
         full_grade_path = os.path.join(full_subject_path, grade_path)
-        
-        downloadable_files = []
-        
+
         for pdf in os.listdir(full_grade_path):
             pdf_paths.append(os.path.join(full_grade_path, pdf))
+        
+        files_used = pdf_paths
+        
+        pdf_paths.append(os.path.join(AZ_STANDARDS, 'base', 'lesson_plan_template.pdf'))
+        # The template provided allows the LM to understand the context of the question and provide an example for a lesson plan.
         
         pdf_texts = []
         raw_text = ''
@@ -65,7 +68,6 @@ with gr.Blocks() as demo:
         
         # convert pdf files to text
         for pdf in pdf_paths:
-            downloadable_files.append(gr.File(value=pdf, label="Context files"))
             try :
                 reader = PdfReader(pdf)
             except:
@@ -100,10 +102,11 @@ with gr.Blocks() as demo:
         for character in bot_message:
             history[-1][1] += character
             # time.sleep(0.05)
-            yield '', history
-    
+            yield '', history, files_used
+
+
     msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-        bot, [subject, grade, msg, chatbot], [msg, chatbot]
+        bot, [subject, grade, msg, chatbot, files_used], [msg, chatbot, files_used]
     )
     clear.click(lambda: None, None, chatbot, queue=False)
 
